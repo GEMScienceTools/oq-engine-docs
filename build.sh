df3571d64b1b530a828c4a69bd122856056e3112
Anirudh Rao <anirudh.rao@globalquakemodel.org>
2015-10-02 19:06:25 +0200

    Add makeindex to build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ pdflatex -interaction=nonstopmode oq-manual.tex &> log.md
 bibtex oq-manual > log.md
 pdflatex -interaction=nonstopmode oq-manual.tex &> log.md
 pdflatex -interaction=nonstopmode oq-manual.tex &> log.md
-makeindex oq-manual.idx -s configuration/StyleInd.ist &> log.md
+makeindex oq-manual.idx
 makeglossaries oq-manual &> log.md
 pdflatex -interaction=nonstopmode oq-manual.tex &> log.md
 cat log.md | egrep "Error|Warning"
