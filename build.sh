714978c2d157b8f38e923210ea753ec9730cc8d7
Anirudh Rao <anirudh.rao@globalquakemodel.org>
2015-11-17 16:02:38 +0100

    Use minted instead of listings for syntax highlighting
    Minted uses Pygments, which provides superior syntax highlighting compared to
    conventional packages. For example, listings basically only highlights
    strings, comments and keywords. Pygments, on the other hand, can be completely
    customized to highlight any kind of token the source language might support.

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-pdflatex -interaction=nonstopmode oq-manual.tex &> log.md
+pdflatex -shell-escape -interaction=nonstopmode oq-manual.tex &> log.md
 bibtex oq-manual > log.md
-pdflatex -interaction=nonstopmode oq-manual.tex &> log.md
-pdflatex -interaction=nonstopmode oq-manual.tex &> log.md
+pdflatex -shell-escape -interaction=nonstopmode oq-manual.tex &> log.md
+pdflatex -shell-escape -interaction=nonstopmode oq-manual.tex &> log.md
 makeindex oq-manual.idx
 makeglossaries oq-manual &> log.md
-pdflatex -interaction=nonstopmode oq-manual.tex &> log.md
+pdflatex -shell-escape -interaction=nonstopmode oq-manual.tex &> log.md
 cat log.md | egrep "Error|Warning"
 ./clean.sh
 open oq-manual.pdf
