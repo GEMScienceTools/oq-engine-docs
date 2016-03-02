730c213585c19c3bd377f90d400165f4c7d86de0
Anirudh Rao <anirudh.rao@globalquakemodel.org>
2016-03-02 11:29:57 +0100

    Update cover page and version number to v1.9

--- clean.sh
+++ clean.sh
@@ -3,6 +3,7 @@ rm *.acr
 rm *.alg
 find . -name \*.aux -delete
 rm *.bbl
+find . -name \*.DS_store -delete
 rm *.glg
 rm *.glo
 rm *.gls
@@ -18,3 +19,4 @@ rm *.run.xml
 find . -name \*.swp -delete
 rm *.toc
 rm log.md
+rm oq-manual-blx.bib
